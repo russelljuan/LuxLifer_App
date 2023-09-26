94434bb3a82e57cda026ed46ab56569b4adb0641
cwmservices <masoodkhan224488@gmail.com>
2023-09-26 13:51:53 -0700

    added auth and stripe to complete this full stack build

---
+++ client/src/api/index.js
@@ -0,0 +1,46 @@
+export const categoriesData = [
+    {
+      id: 1,
+      img: "/images/categories.png",
+      title: "All Categories",
+    },
+    {
+      id: 2,
+      img: "/images/tinyandteens.png",
+      title: "Tiny,Todd's,&Teens Collection",
+    },
+    {
+      id: 3,
+      img: "/images/unclecollection.png",
+      title: "Uncle 42 Collection",
+    },
+    {
+      id: 4,
+      img: "/images/sneakers.png",
+      title: "Sneakers",
+    },
+  
+    {
+      id: 5,
+      img: "/images/caps.png",
+      title: "Caps",
+    },
+    {
+      id: 6,
+      img: "/images/shirts.png",
+      title: "Shirts",
+    },
+    {
+      id: 7,
+      img: "/images/hoodies.png",
+      title: "Hoodies",
+    },
+    {
+      id: 8,
+      img: "/images/toys.png",
+      title: "Toys",
+    }
+  ];
+
+  export const apiURL = "https://johnstore.cyclic.cloud";
+  // export const apiURL = "http://localhost:5000"
